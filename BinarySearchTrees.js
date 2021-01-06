3804f407a25bf0c2d5d2f96e251ff6d4809b9929
Redwan <redwan.mahmud07@gmail.com>
2021-01-05 21:46:34 -0800

    Added DFS InOrder,PreOrder, PostOrder

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -69,30 +69,71 @@ class BinarySearchTree{
         return current.value
 
     }
-
-    BFS(){
-        var data = [].
+    
+    BFS(){ //breadth first search 
+        var data = [],
             queue = [],
-            node = this.root
-
-        queue.push(node)
+            node = this.root //first element 
+        queue.push(node) // 
         while(queue.length){
-            node = queue.shift() 
-            data.push(node)
-            if(node.left) queue.push(node.left)
-            if(node.right) queue.push(node.right)
+            node = queue.shift()
+            console.log(node.value)
+            data.push(node.value)                      //1st iter- adds to list| 2nd iter  
+            if(node.left) queue.push(node.left) //6 gets added          |
+            if(node.right) queue.push(node.right) //15 gets added        |
         }
-        return data  
+        return data;
+    }
+
+    DFSPreOrder(){
+        var data = []
+        var current = this.root
+        function traverse(node){
+            data.push(node.value)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+        traverse(current)
+        return data 
+    }
+
+    DFSPostOrder(){
+        var data = []
+        var current = this.root 
+        function traverse(node){
+            if(node.left) traverse(node.left)
+            if(node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(current)
+        return data 
+    }
+    DFSInOrder(){
+        var data = []
+        var current = this.root 
+        function traverse(node){
+            if(node.left) traverse(node.left)
+            data.push(node.value)
+            if(node.right) traverse(node.right)       
+        }
+        traverse(current)
+        return data 
     }
 }
 
-var tree = new BinarySearchTree()
+//              10
+//          6        15      
+//      3      8         20
+// queue  []
+// data [10,6,15,3,8,20] BFS 
 
-tree.insert(10)
-tree.insert(5)
-tree.insert(12)
-tree.insert(13)
-tree.insert(11)
-tree.insert(4)
-console.log(tree.find(1))
+var t = new BinarySearchTree()
+t.insert(10)
+t.insert(6)
+t.insert(15)
+t.insert(3)
+t.insert(8)
+t.insert(20)
+
+console.log(t.DFSInOrder())
 //console.log(tree)
