6c6daacb67c92d3755efd8a61cc8fb1d7f84ed5e
redwan-mahmud <redwan.mahmud@icloud.com>
2021-01-05 16:02:03 -0800

    Added BFS

--- BinarySearchTrees.js
+++ BinarySearchTrees.js
@@ -69,6 +69,21 @@ class BinarySearchTree{
         return current.value
 
     }
+
+    BFS(){
+        var data = [].
+            queue = [],
+            node = this.root
+
+        queue.push(node)
+        while(queue.length){
+            node = queue.shift() 
+            data.push(node)
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return data  
+    }
 }
 
 var tree = new BinarySearchTree()
