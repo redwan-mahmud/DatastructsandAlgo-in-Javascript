24a3732e20ed389e723a848cc3ed44219168d538
redwan-mahmud <redwan.mahmud@icloud.com>
2021-01-26 15:46:38 -0800

    Solved max array using Kadan'es approach

--- leetcode/MaximumSubArray.js
+++ leetcode/MaximumSubArray.js
@@ -19,5 +19,17 @@ Follow up: If you have figured out the O(n) solution, try coding another solutio
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    var curr_max = 0;
+    var max_so_far = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        curr_max = Math.max(nums[i], curr_max + nums[i]);
+        max_so_far = Math.max(curr_max, max_so_far);
+    }
+
+    return max_so_far
 };
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+// implemented using Kadane's approach 
